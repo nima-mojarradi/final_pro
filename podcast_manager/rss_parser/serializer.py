091a99be1bc2d6e33a416854e312b7f15be985b1
nima-mojarradi <nima.mojarradi83@gmail.com>
2023-09-14 13:26:45 +0430

    chor:name of serializer changed

--- podcast_manager/rss_parser/serializer.py
+++ podcast_manager/rss_parser/serializer.py
@@ -2,8 +2,10 @@ from rest_framework import serializers
 from .models import ModelParser
 
 class ModelParserSerializer(serializers.ModelSerializer):
-    description = serializers.CharField(read_only=True)
-    title = serializers.CharField(read_only=True)
     class Meta:
         model = ModelParser
         fields = ('link','description','title')
+        extra_kwargs = {
+            'description':{'read_only':True},
+            'title':{'read_only':True}
+        }
