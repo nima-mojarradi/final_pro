c3ee1d67b6060d6c6bd2979fb49846017f7786c1
nima-mojarradi <nima.mojarradi83@gmail.com>
2023-09-15 08:23:22 +0430

    chor:some extra spaces removed from file

--- podcast_manager/rss_parser/models.py
+++ podcast_manager/rss_parser/models.py
@@ -4,4 +4,3 @@ class ModelParser(models.Model):
     link = models.URLField(null=True,blank=True)
     title = models.CharField(max_length=255)
     description = models.TextField()
-    pub_date = models.DateTimeField(null=True,blank=True)
