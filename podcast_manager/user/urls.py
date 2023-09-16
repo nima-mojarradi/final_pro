960c6e47e2960c2ca641306c58c3ea18f412b075
nima-mojarradi <nima.mojarradi83@gmail.com>
2023-09-16 12:06:48 +0430

    feat:erd and requirements.txt file added

--- podcast_manager/user/urls.py
+++ podcast_manager/user/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import RegisterUserView, user_login
+from .views import RegisterUserView, LoginUserView
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
-    path('login/', user_login, name='login'),
+    path('login/', LoginUserView.as_view(), name='login'),
 ]
--- podcast_manager/user/views.py
+++ podcast_manager/user/views.py
@@ -6,7 +6,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
-
+from rest_framework.generics import CreateAPIView
 
 class RegisterUserView(APIView):
     def get(self, request):
@@ -18,19 +18,18 @@ class RegisterUserView(APIView):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
-@csrf_exempt
-@api_view(['GET'])
-def user_login(request):
-    username = request.data.get('username')
-    password = request.data.get('password')
-    if not username or not password:
-        return JsonResponse({'error': 'Please provide username and password'}, status=status.HTTP_400_BAD_REQUEST)
-    user = authenticate(username=username, password=password)
-    if user is not None:
-        login(request, user)
-        return JsonResponse({'success': 'User logged in successfully'}, status=status.HTTP_200_OK)
-    else:
-        return JsonResponse({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+class LoginUserView(CreateAPIView):
+    def get(self,request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        if not username or not password:
+            return JsonResponse({'error': 'Please provide username and password'}, status=status.HTTP_400_BAD_REQUEST)
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return JsonResponse({'success': 'User logged in successfully'}, status=status.HTTP_200_OK)
+        else:
+            return JsonResponse({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
     
 
 
