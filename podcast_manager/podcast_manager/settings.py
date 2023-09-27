b680b3de9c025503c4dc30c2d4c81f8219793e26
nima-mojarradi <nima.mojarradi83@gmail.com>
2023-09-27 11:31:21 +0330

    feat:settings configured

--- podcast_manager/podcast_manager/settings.py
+++ podcast_manager/podcast_manager/settings.py
@@ -85,18 +85,18 @@ WSGI_APPLICATION = 'podcast_manager.wsgi.application'
 
 DATABASES = {
     'default': 
-#     {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-   {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('NAME'),
-        'USER': os.environ.get('USER'),
-        'PASSWORD': os.environ.get('PASSWORD'),
-        'HOST': os.environ.get('HOST'),
-        'PORT': os.environ.get('PORT')
-    }
+    {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+   }
+#    {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('NAME'),
+#         'USER': os.environ.get('USER'),
+#         'PASSWORD': os.environ.get('PASSWORD'),
+#         'HOST': os.environ.get('HOST'),
+#         'PORT': os.environ.get('PORT')
+#     }
 }
 
 
