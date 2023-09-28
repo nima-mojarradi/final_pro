35a434d0cc1050c6bf6c619deb298ea26862aaad
nima-mojarradi <nima.mojarradi83@gmail.com>
2023-09-28 16:32:46 +0330

    feat:view for liked view added

--- podcast_manager/rss_parser/views.py
+++ podcast_manager/rss_parser/views.py
@@ -3,22 +3,16 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from .serializer import LikedPodcastsSerializer
 from .models import Like, ModelParser
 
-class LikedPosts(APIView):
+class LikedPodcasts(APIView):
     authentication_classes = [SessionAuthentication, BasicAuthentication]
     permission_classes = [IsAuthenticated]
-
     def get(self, request):
         user = request.user
-        print(user)
-        liked_post = Like.objects.filter(user=user)
-        if liked_post:
-            podcast_ids = [like.podcast.id for like in liked_post]
-            podcasts = ModelParser.objects.filter(id__in=podcast_ids)
-            return Response(podcasts)
-        else:
-            return Response([])
-
+        liked_podcasts = Like.objects.filter(user=user)
+        serializer = LikedPodcastsSerializer(liked_podcasts, many=True)
+        return Response(serializer.data)
 
 
